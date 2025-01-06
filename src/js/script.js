f80e13930da9639a9318c08e7e66bba27982c89a
yuliagusmy <yuliagusmy@gmail.com>
2025-01-06 10:34:38 +0800

    Update header and about

--- src/js/script.js
+++ src/js/script.js
@@ -221,3 +221,9 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
+        function scrollToPortfolio(id) {
+            const element = document.getElementById(id);
+            if (element) {
+                element.scrollIntoView({ behavior: "smooth" });
+            }
+        }
